8acd51cfe8dc295e4cb38e0032f30b7dbe7d1abe
streambrother <852610613@qq.com>
2019-11-01 14:17:06 +0800

    sp02第一次

---
+++ sp02-itemservice/src/test/java/com/cy/sp02/Sp02ItemserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cy.sp02;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp02ItemserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
